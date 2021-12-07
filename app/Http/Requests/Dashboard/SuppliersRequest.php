5f6a8c05bf9fc4bf06b42fd157ddf5f67c872a04
MostafaAlii <mostafa0alii@gmail.com>
2021-12-07 13:16:27 +0200

    finish suppliers add validation

--- app/Http/Requests/Dashboard/SuppliersRequest.php
+++ app/Http/Requests/Dashboard/SuppliersRequest.php
@@ -8,10 +8,38 @@ class SuppliersRequest extends FormRequest
         return true;
     }
 
-    public function messages()
+    public function rules()
     {
         return [
-            
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email|unique:suppliers,email,',
+            'password'  => 'required|confirmed|min:6',
+            'password_confirmation' =>  'required',
+            'phone'     =>  'required|numeric|min:10|max:11',
+        ];
+    }
+
+    public function messages()
+    {
+        return  [
+            'first_name.required'             =>  trans('dashboard/supplier.first_name_required'),
+            'first_name.string'             =>  trans('dashboard/supplier.first_name_string'),
+            'last_name.required'             =>  trans('dashboard/supplier.last_name_required'),
+            'last_name.string'             =>  trans('dashboard/supplier.last_name_string'),
+
+            'email.required'        =>  trans('dashboard/supplier.email_required'),
+            'email.email'           =>  trans('dashboard/supplier.email_true'),
+            'email.unique'              =>  trans('dashboard/supplier.email_unique'),
+
+            'password.required'         =>  trans('dashboard/supplier.password_required'),
+            'password.confirmed'         =>  trans('dashboard/supplier.password_confirmed'),
+            'password_confirmation.required'     =>  trans('dashboard/supplier.password_confirmation_required'),
+
+            'phone.required'        =>  trans('dashboard/supplier.phone_required'),
+            'phone.max'             =>  trans('dashboard/supplier.phone_max'),
+            'phone.min'             =>  trans('dashboard/supplier.phone_min'),
+            'phone.numeric'             =>  trans('dashboard/supplier.pohne_numeric'),
         ];
     }
 }
--- database/migrations/2021_12_05_141359_create_suppliers_table.php
+++ database/migrations/2021_12_05_141359_create_suppliers_table.php
@@ -18,7 +18,7 @@ class CreateSuppliersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
-            $table->text('address');
+            $table->text('address')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
